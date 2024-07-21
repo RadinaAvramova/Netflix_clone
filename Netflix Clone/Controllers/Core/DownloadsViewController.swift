0871c0ffbac2365fe3524b84c84d41eb28c1ecc7
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-07-21 10:51:51 +0300

    Update DownloadsViewController.swift

--- Netflix Clone/Controllers/Core/DownloadsViewController.swift
+++ Netflix Clone/Controllers/Core/DownloadsViewController.swift
@@ -1,11 +1,4 @@
-//
-//  DownloadsViewController.swift
-//  Netflix Clone
-//
-//  Created by Amr Hossam on 04/11/2021.
-//
-
-import UIKit
+IKit
 
 class DownloadsViewController: UIViewController {
     
